943ffceccb20485b1ff43f5805bdd63b5e7ae9d3
KhinYadanaThein <angelchit35@gmail.com>
2018-06-15 22:56:48 +0530

    added hello.py

--- hello.py
+++ hello.py
@@ -4,7 +4,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return render_template("index.html")
+    return """
+    <html>
+    <head><title>This is my flask app</title></head>
+    <body><h1>Hello from Flask </h1></body>
+    </html>
+    """
 
 if __name__ == "__main__":
     app.run(debug=True)
